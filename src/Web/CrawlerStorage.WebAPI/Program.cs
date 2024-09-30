5796eee426ddcf95d1cf3859ea8481f4d81fbc55
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-09-30 13:02:46 +0300

    Refactoring.

--- src/Web/CrawlerStorage.WebAPI/Program.cs
+++ src/Web/CrawlerStorage.WebAPI/Program.cs
@@ -19,7 +19,6 @@ builder.Services.AddApiVersioning(options =>
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(setup =>
 {
@@ -29,7 +28,6 @@ builder.Services.AddSwaggerGen(setup =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
