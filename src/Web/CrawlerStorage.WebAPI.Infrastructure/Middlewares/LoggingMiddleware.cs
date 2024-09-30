c3eec1f225df2b571bbdb9418de72a50af8698b6
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-09-30 14:05:30 +0300

    Created middleware for logging errors.

---
+++ src/Web/CrawlerStorage.WebAPI.Infrastructure/Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,31 @@
+﻿namespace CrawlerStorage.WebAPI.Infrastructure.Middlewares;
+
+using Microsoft.AspNetCore.Http;
+
+public class LoggingMiddleware
+{
+    private readonly RequestDelegate next;
+
+    public LoggingMiddleware(RequestDelegate next)
+    {
+        this.next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await this.next(context);
+        }
+        catch (Exception ex)
+        {
+            await this.LogError(ex);
+            throw;
+        }
+    }
+
+    private async Task LogError(Exception ex)
+    {
+        throw new NotImplementedException();
+    }
+}
