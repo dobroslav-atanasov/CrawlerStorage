6031c113394a26de7f0eb9f9cecef93c7b7dc8bf
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-09-30 10:42:02 +0300

    Create dbcontext, installed nuget packages and added reference.

---
+++ src/Data/CrawlerStorage.Data/CrawlerStorageDbContext.cs
@@ -0,0 +1,28 @@
+﻿namespace CrawlerStorage.Data;
+
+using CrawlerStorage.Data.Models.DbEntities;
+
+using Microsoft.EntityFrameworkCore;
+
+public class CrawlerStorageDbContext : DbContext
+{
+    public CrawlerStorageDbContext(DbContextOptions<CrawlerStorageDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Crawler> Crawlers { get; set; }
+
+    public virtual DbSet<Document> Documents { get; set; }
+
+    public virtual DbSet<Group> Groups { get; set; }
+
+    public virtual DbSet<Log> Logs { get; set; }
+
+    public virtual DbSet<Operation> Operations { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
