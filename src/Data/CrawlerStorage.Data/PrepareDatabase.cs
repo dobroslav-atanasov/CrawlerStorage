2e4e37d5cd140e7d44529b2d21f6d9f5767f954a
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-10-02 15:28:33 +0300

    Created static class to prepare database.

---
+++ src/Data/CrawlerStorage.Data/PrepareDatabase.cs
@@ -0,0 +1,18 @@
+﻿namespace CrawlerStorage.Data;
+
+using System;
+
+using Microsoft.EntityFrameworkCore;
+
+public static class PrepareDatabase
+{
+    public static void Migrate(CrawlerStorageDbContext context)
+    {
+        context.Database.Migrate();
+    }
+
+    private static void SeedData(CrawlerStorageDbContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
