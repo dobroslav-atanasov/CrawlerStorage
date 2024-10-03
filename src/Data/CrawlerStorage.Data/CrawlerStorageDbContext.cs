d89039b7b30fc7474a03898178b048baf8a42d58
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-10-03 14:49:56 +0300

    Database context override save changes.

--- src/Data/CrawlerStorage.Data/CrawlerStorageDbContext.cs
+++ src/Data/CrawlerStorage.Data/CrawlerStorageDbContext.cs
@@ -1,5 +1,6 @@
 ﻿namespace CrawlerStorage.Data;
 
+using CrawlerStorage.Data.Common.Interfaces;
 using CrawlerStorage.Data.Models.DbEntities;
 
 using Microsoft.EntityFrameworkCore;
@@ -20,6 +21,48 @@ public class CrawlerStorageDbContext : DbContext
     public virtual DbSet<Log> Logs { get; set; }
 
     public virtual DbSet<Operation> Operations { get; set; }
+
+    public override int SaveChanges()
+    {
+        return this.SaveChanges(true);
+    }
+
+    public override int SaveChanges(bool acceptAllChangesOnSuccess)
+    {
+        this.ApplyCheckRules();
+        return base.SaveChanges(acceptAllChangesOnSuccess);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return this.SaveChangesAsync(true, cancellationToken);
+    }
+
+    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+    {
+        this.ApplyCheckRules();
+        return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+    }
+
+    private void ApplyCheckRules()
+    {
+        var changedEntries = this.ChangeTracker
+                .Entries()
+                .Where(e => e.Entity is ICheckableEntity && (e.State == EntityState.Added || e.State == EntityState.Modified));
+
+        foreach (var entry in changedEntries)
+        {
+            var entity = (ICheckableEntity)entry.Entity;
+            if (entry.State == EntityState.Added && entity.CreatedOn == default)
+            {
+                entity.CreatedOn = DateTime.UtcNow;
+            }
+            else
+            {
+                entity.ModifiedOn = DateTime.UtcNow;
+            }
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
