2e60790517d4265484f2c003e9f4d9b01438982a
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-10-03 14:50:32 +0300

    Added properties.

--- src/Data/CrawlerStorage.Data.Models/Dtos/Documents/DocumentDto.cs
+++ src/Data/CrawlerStorage.Data.Models/Dtos/Documents/DocumentDto.cs
@@ -2,4 +2,23 @@
 
 public class DocumentDto
 {
+    public Guid Identifier { get; set; }
+
+    public string Name { get; set; }
+
+    public string Format { get; set; }
+
+    public int Order { get; set; }
+
+    public string Url { get; set; }
+
+    public int Operation { get; set; }
+
+    public string Encoding { get; set; }
+
+    public string MD5 { get; set; }
+
+    public int GroupId { get; set; }
+
+    public byte[] Content { get; set; }
 }
