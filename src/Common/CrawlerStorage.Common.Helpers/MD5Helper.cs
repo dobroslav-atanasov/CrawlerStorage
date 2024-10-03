dcd4069ded5447a89f6552bc411da1f50a9d39c8
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-10-03 16:08:10 +0300

    Created MD5Helper and ZipHelper.

---
+++ src/Common/CrawlerStorage.Common.Helpers/MD5Helper.cs
@@ -0,0 +1,22 @@
+﻿namespace CrawlerStorage.Common.Helpers;
+
+using System.Security.Cryptography;
+using System.Text;
+
+public static class MD5Helper
+{
+    public static string Hash(byte[] data)
+    {
+        using var md5 = MD5.Create();
+
+        var hashBytes = md5.ComputeHash(data);
+
+        var sb = new StringBuilder();
+        for (var i = 0; i < hashBytes.Length; i++)
+        {
+            sb.Append(hashBytes[i].ToString("X2"));
+        }
+
+        return sb.ToString();
+    }
+}
---
+++ src/Common/CrawlerStorage.Common.Helpers/ZipHelper.cs
@@ -0,0 +1,76 @@
+﻿namespace CrawlerStorage.Common.Helpers;
+
+using CrawlerStorage.Data.Models.DbEntities;
+using CrawlerStorage.Data.Models.Zip;
+
+using ICSharpCode.SharpZipLib.Zip;
+
+public static class ZipHelper
+{
+    public static List<ZipModel> UnzipGroup(byte[] data)
+    {
+        var zipModels = new List<ZipModel>();
+
+        using var memoryStream = new MemoryStream(data);
+        using var zipFile = new ZipFile(memoryStream);
+
+        foreach (ZipEntry zipEntry in zipFile)
+        {
+            if (!zipEntry.IsFile)
+            {
+                continue;
+            }
+
+            var fileName = zipEntry.Name;
+            var buffer = new byte[4096];
+            var zipStream = zipFile.GetInputStream(zipEntry);
+
+            using var ms = new MemoryStream();
+
+            int read;
+            while ((read = zipStream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                ms.Write(buffer, 0, read);
+            }
+
+            var zipModel = new ZipModel
+            {
+                Name = fileName,
+                Content = ms.ToArray()
+            };
+
+            zipModels.Add(zipModel);
+        }
+
+        return zipModels;
+    }
+
+    public static byte[] ZipGroup(Group group)
+    {
+        using var memoryStream = new MemoryStream();
+        using var zipOutputStream = new ZipOutputStream(memoryStream);
+
+        zipOutputStream.SetLevel(9);
+        var buffer = new byte[4096];
+
+        foreach (var document in group.Documents)
+        {
+            var entry = new ZipEntry(Path.GetFileName(document.Name));
+            zipOutputStream.PutNextEntry(entry);
+
+            using var ms = new MemoryStream(document.Content, 0, document.Content.Length);
+
+            int bytes;
+            do
+            {
+                bytes = ms.Read(buffer, 0, buffer.Length);
+                zipOutputStream.Write(buffer, 0, bytes);
+            }
+            while (bytes > 0);
+        }
+
+        zipOutputStream.Finish();
+
+        return memoryStream.ToArray();
+    }
+}
