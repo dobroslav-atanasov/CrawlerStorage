11bdb339d901da954cebb1d2c0d1ad4574be101e
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-10-08 10:53:30 +0300

    Implemented endpoints to get document and documents by groupId.

--- src/Web/CrawlerStorage.WebAPI/Controllers/DocumentsController.cs
+++ src/Web/CrawlerStorage.WebAPI/Controllers/DocumentsController.cs
@@ -8,6 +8,7 @@ using CrawlerStorage.Data.Repositories;
 
 using Microsoft.AspNetCore.Mvc;
 
+[Route("api/v{version:apiVersion}/Groups/{groupId}/[controller]")]
 public class DocumentsController : BaseController
 {
     private readonly IMapper mapper;
@@ -19,15 +20,34 @@ public class DocumentsController : BaseController
         this.repository = repository;
     }
 
-    [HttpGet("{id}", Name = nameof(GetDocumentsByGroupId))]
-    public async Task<IActionResult> GetDocumentsByGroupId(int id)
+    [HttpGet(Name = nameof(GetDocumentsByGroupId))]
+    public async Task<IActionResult> GetDocumentsByGroupId(int groupId)
     {
-        var group = await this.repository.FindAsync(id);
+        var group = await this.repository.FindAsync(groupId);
 
         if (group != null)
         {
             var documentReadDtos = this.mapper.Map<IEnumerable<DocumentReadDto>>(group.Documents);
             return this.Ok(documentReadDtos);
+        }
+
+        return this.NotFound();
+    }
+
+    [HttpGet("{id}", Name = nameof(GetDocumentByGroupId))]
+    public async Task<IActionResult> GetDocumentByGroupId(int groupId, int id)
+    {
+        var group = await this.repository.FindAsync(groupId);
+
+        if (group != null)
+        {
+            var document = group.Documents.FirstOrDefault(x => x.Id == id);
+            if (document != null)
+            {
+                var documentReadDto = this.mapper.Map<DocumentReadDto>(document);
+
+                return this.Ok(documentReadDto);
+            }
         }
 
         return this.NotFound();
