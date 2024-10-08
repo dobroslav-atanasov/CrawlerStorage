50ee1acf81be956f6cbb624951a19e2746258e1e
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-10-08 09:57:42 +0300

    Created map between DocumentReadDto and Document.

--- src/Services/CrawlerStorage.Services.Automapper/Profiles/DocumentsProfile.cs
+++ src/Services/CrawlerStorage.Services.Automapper/Profiles/DocumentsProfile.cs
@@ -11,5 +11,7 @@ public class DocumentsProfile : Profile
     {
         this.CreateMap<DocumentDto, Document>()
             .ReverseMap();
+
+        this.CreateMap<Document, DocumentReadDto>();
     }
 }
