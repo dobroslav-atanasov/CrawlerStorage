dd0948b00259edd40f07c3e8db4029ae3b84123c
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-10-08 09:58:20 +0300

    Implemented endpoint in documents controller.

--- src/Web/CrawlerStorage.WebAPI/Controllers/DocumentsController.cs
+++ src/Web/CrawlerStorage.WebAPI/Controllers/DocumentsController.cs
@@ -1,6 +1,35 @@
 ﻿namespace CrawlerStorage.WebAPI.Controllers;
 
+using AutoMapper;
+
+using CrawlerStorage.Data.Models.DbEntities;
+using CrawlerStorage.Data.Models.Dtos.Documents;
+using CrawlerStorage.Data.Repositories;
+
+using Microsoft.AspNetCore.Mvc;
+
 public class DocumentsController : BaseController
 {
+    private readonly IMapper mapper;
+    private readonly CrawlerStorageRepository<Group> repository;
 
+    public DocumentsController(IMapper mapper, CrawlerStorageRepository<Group> repository)
+    {
+        this.mapper = mapper;
+        this.repository = repository;
+    }
+
+    [HttpGet("{id}", Name = nameof(GetDocumentsByGroupId))]
+    public async Task<IActionResult> GetDocumentsByGroupId(int id)
+    {
+        var group = await this.repository.FindAsync(id);
+
+        if (group != null)
+        {
+            var documentReadDtos = this.mapper.Map<IEnumerable<DocumentReadDto>>(group.Documents);
+            return this.Ok(documentReadDtos);
+        }
+
+        return this.NotFound();
+    }
 }
