950a7e989798ad82e2a6517d1f39d470c29c8e2a
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-10-12 07:58:52 +0300

    Created rabbitmq options model.

---
+++ src/Data/CrawlerStorage.Data.Models/Settings/RabbitMQModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CrawlerStorage.Data.Models.Settings;
+
+public class RabbitMQModel
+{
+    public string Host { get; set; }
+
+    public int Port { get; set; }
+
+    public string User { get; set; }
+
+    public string Password { get; set; }
+}
