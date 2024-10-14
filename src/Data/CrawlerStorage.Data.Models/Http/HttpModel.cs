b52ce5975a224f5fba17209c78f7ccfe97df40b8
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2024-10-14 09:31:02 +0300

    Created http model.

---
+++ src/Data/CrawlerStorage.Data.Models/Http/HttpModel.cs
@@ -0,0 +1,24 @@
+﻿namespace CrawlerStorage.Data.Models.Http;
+
+using System.Text;
+
+public class HttpModel
+{
+    public string Name { get; set; }
+
+    public string Url { get; set; }
+
+    public Uri Uri => new Uri(this.Url, UriKind.RelativeOrAbsolute);
+
+    //public byte[] Bytes => this.Encoding.GetBytes(this.Content);
+
+    public Encoding Encoding { get; set; }
+
+    public byte[] Content { get; set; }
+
+    public string MimeType { get; set; }
+
+    //    public HtmlDocument HtmlDocument { get; set; }
+
+    //    public HttpStatusCode StatusCode { get; set; }
+}
